695d3b7a1ed3e3439746f1c4c4374c4b8583f06e
Tobias Andersen <tobias.andersen79@gmail.com>
2024-01-22 19:33:15 +0100

    baseline migrations + docker-compose w. postgres

---
+++ src/Heimdall.Templates.Dotnet.Microservice.Infrastructure/EntityFramework/Migrations/20240122183232_baseline.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Heimdall.Templates.Dotnet.Microservice.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class baseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DomainEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DomainEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Objects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Label = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    CapabilityIdentifier = table.Column<string>(type: "text", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Objects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Objects_DomainEntity_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "DomainEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Objects_OwnerId",
+                table: "Objects",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Objects");
+
+            migrationBuilder.DropTable(
+                name: "DomainEntity");
+        }
+    }
+}
